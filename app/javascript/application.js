5198b4951ade1ad3835359792e498475e7b069a9
Rodrigo Fernández <rofaccess@gmail.com>
2023-08-22 20:53:09 -0400

    #1 Remove manual install of Mazer template

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
-//= require mazer/dark
-//= require mazer/app
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
