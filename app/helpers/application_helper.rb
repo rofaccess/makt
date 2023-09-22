efefd76cd7e1faac173898915f309113ae005650
Rodrigo Fernández <rofaccess@gmail.com>
2023-09-22 18:28:24 -0400

    #7 Add favicon and logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def img_color
+    # Alternatives: blue, green, lilac
+    "lilac"
+  end
+
+  def favicon_tag
+    "<link rel='icon' type='image/x-icon' href='../favicon_#{img_color}.ico'>".html_safe
+  end
+
+  def logo_tag
+    "<a href='#'><img src='../logo_#{img_color}.png' alt='MaKT'></a>".html_safe
+  end
 end
