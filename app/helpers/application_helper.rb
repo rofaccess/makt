cc295babfdf6f1af99a1884c66622966cbd8a7b9
Rodrigo Fernández <rofaccess@gmail.com>
2023-09-22 21:21:40 -0400

    #12 Improve forgot password view. Move images in correct folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,19 @@ module ApplicationHelper
     "lilac"
   end
 
+  def logo_name
+    "logo_#{img_color}.png"
+  end
+
+  def favicon_name
+    "favicon_#{img_color}.ico"
+  end
+
   def favicon_tag
-    "<link rel='icon' type='image/x-icon' href='../favicon_#{img_color}.ico'>".html_safe
+    "<link rel='icon' type='image/x-icon' href='#{image_path(favicon_name)}'>".html_safe
   end
 
   def logo_tag
-    "<a href='#'><img src='../logo_#{img_color}.png' alt='MaKT'></a>".html_safe
+    "<a href='#'><img src='#{image_path(logo_name)}' alt='MaKT'></a>".html_safe
   end
 end
