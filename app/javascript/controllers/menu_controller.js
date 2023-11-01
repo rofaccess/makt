a261263323fbe2343edb50057a21b9d748b53279
Rodrigo Fernández <rofaccess@gmail.com>
2023-11-01 11:28:18 -0300

    #20 Improve menu item hover animation. Use rem instead px in sizes

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -3,17 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   activateMenuItem(event) {
     const menuItem = this.#getMenuItem(event.target)
-    //debugger
 
     // Not is necessary manage active links when turbo is disabled
-    if(this.#turboDisabled(menuItem)) return;
+    if(this.#turboDisabled(menuItem)) return
 
     this.#deactivateMenuItems(this.element)
-    console.log(this.element)
     this.#activateMenuItem(menuItem)
 
     this.#updateUrl(menuItem)
-    console.log(menuItem)
   }
 
   #turboDisabled(menuItem) {
@@ -44,6 +41,6 @@ export default class extends Controller {
     const nextState = { additionalInformation: "Updated the URL with JS" }
 
     // This will create a new entry in the browser's history, without reloading
-    window.history.pushState(nextState, nextTitle, nextURL);
+    window.history.pushState(nextState, nextTitle, nextURL)
   }
 }
